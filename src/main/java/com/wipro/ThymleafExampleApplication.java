17c2df5345ddc12f9c3c11f8d3b2989c6927f3a6
Amit Sharma <ksharma.amit2022@gmail.com>
2022-12-01 12:58:50 +0530

    comit

---
+++ src/main/java/com/wipro/ThymleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.wipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wipro/thymleafExample/ThymleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.thymleafExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
